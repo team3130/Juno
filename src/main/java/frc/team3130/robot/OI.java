9f1a2496dba0604652859e6c3c937b58de057c1e
Tantan4321 <kupa528@yahoo.com>
2019-01-09 17:30:06 -0600

    Create OI

---
+++ src/main/java/frc/team3130/robot/OI.java
@@ -0,0 +1,70 @@
+package frc.team3130.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Trigger;
+
+public class OI {
+    private class JoystickTrigger extends Trigger {
+
+        private Joystick stick;
+        private int axis;
+        private double threshold;
+
+        private JoystickTrigger(Joystick stick, int axis){
+            this.stick = stick;
+            this.axis = axis;
+            threshold = 0.1;
+        }
+
+        private JoystickTrigger(Joystick stick, int axis, double threshold){
+            this.stick = stick;
+            this.axis = axis;
+            this.threshold = threshold;
+        }
+
+        @Override
+        public boolean get() {
+            return stick.getRawAxis(axis) > threshold;
+        }
+
+    }
+
+    private class POVTrigger extends Trigger{
+
+        private Joystick stick;
+        private int POV;
+
+        public POVTrigger(Joystick stick, int POV) {
+            this.stick = stick;
+            this.POV = POV;
+        }
+
+        @Override
+        public boolean get() {
+            return stick.getPOV(0)==POV;
+        }
+
+    }
+
+    //Instance Handling
+    private static OI m_pInstance;
+    public static OI GetInstance()
+    {
+        if(m_pInstance == null) m_pInstance = new OI();
+        return m_pInstance;
+    }
+
+    /**
+     * Definitions for joystick buttons start
+     */
+    //Joystick
+    public static Joystick stickL;
+    public static Joystick stickR;
+    public static Joystick gamepad;
+
+    private OI(){
+        stickL = new Joystick(0);
+        stickR = new Joystick(1);
+        gamepad = new Joystick(2);
+    }
+}
