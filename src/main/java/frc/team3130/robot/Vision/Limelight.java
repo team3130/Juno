f9ae9f87b89e7e5640ffc811da9e65411c801de8
awuebben <46405528+awuebben@users.noreply.github.com>
2019-01-15 19:19:18 -0600

    Update Limelight.java

--- src/main/java/frc/team3130/robot/Vision/Limelight.java
+++ src/main/java/frc/team3130/robot/Vision/Limelight.java
@@ -18,7 +18,17 @@ public class Limelight {
     double area = ta.getDouble(0.0);
 
     //post to smart dashboard periodically
-SmartDashboard.putNumber("LimelightX", x);
-SmartDashboard.putNumber("LimelightY", y);
-SmartDashboard.putNumber("LimelightArea", area);
+    SmartDashboard.putNumber("LimelightX", x);
+    SmartDashboard.putNumber("LimelightY", y);
+    SmartDashboard.putNumber("LimelightArea", area);
+
+    double targetOffsetAngle_Horizontal = table.getNumber("tx", 0);
+    double targetOffsetAngle_Vertical = table.getNumber ("ty", 0);
+    double targetArea = table.getNumber ("ta", 0);
+    double targetSkew = table.getNumber ("ts", 0);
+    
+    NetworkTableInstance.getDefault().getTable("limelight").getEntry("<variablename>").getDouble(0);
+
+    NetworkTableInstance.getDefault().getTable("limelight").getEntry("<variablename>").setNumber(<value>);
+
 }
