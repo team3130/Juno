e2bf69a9600c510c7ea6d3687c1a2b7b741894cb
Tantan4321 <kupa528@yahoo.com>
2019-01-27 12:05:11 -0600

    Fix intake toggler

--- src/main/java/frc/team3130/robot/commands/ToggleIntakeSolenoid1.java
+++ src/main/java/frc/team3130/robot/commands/ToggleIntakeSolenoid1.java
@@ -16,13 +16,11 @@ public class ToggleIntakeSolenoid1 extends Command {
     }
 
     // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-        TestIntake.toggleSolenoid1();
-    }
+    protected void execute() { }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return true;
     }
 
     // Called once after isFinished returns true
--- src/main/java/frc/team3130/robot/commands/ToggleIntakeSolenoid2.java
+++ src/main/java/frc/team3130/robot/commands/ToggleIntakeSolenoid2.java
@@ -17,12 +17,12 @@ public class ToggleIntakeSolenoid2 extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-        TestIntake.toggleSolenoid2();
+
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return true;
     }
 
     // Called once after isFinished returns true
