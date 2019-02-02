be3aa243894122b3dc9a93a6a2b4a92607db6a26
Tantan4321 <kupa528@yahoo.com>
2019-02-01 19:54:56 -0600

    Push limelight data to dashboard
    - also handle no target case

--- src/main/java/frc/team3130/robot/Vision/Limelight.java
+++ src/main/java/frc/team3130/robot/Vision/Limelight.java
@@ -1,4 +1,4 @@
-package frc.team3130.robot.Vision;
+package frc.team3130.robot.vision;
 
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
@@ -8,29 +8,40 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Limelight {
 
+    private static NetworkTableEntry tv;
 
-    private static NetworkTableEntry tx ;
-    private static NetworkTableEntry ty ;
-    private static NetworkTableEntry ta ;
+    private static NetworkTableEntry tx; //x angle offset from crosshair, range of -27 to 27
+    private static NetworkTableEntry ty; //y angle offset from crosshari, range of -20.5 to 20.5
+    private static NetworkTableEntry ta;
+
+    private static double x = 0.0;
+    private static double y = 0.0;
+    private static double area = 0.0;
+
     public Limelight() {
         NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+        tv = table.getEntry("tv");
         tx = table.getEntry("tx");
         ty = table.getEntry("ty");
         ta = table.getEntry("ta");
     }
+
+
     //read values periodically
-
-
     public static void updateData() {
-        double x = tx.getDouble(0.0);
-        double y = ty.getDouble(0.0);
-        double area = ta.getDouble(0.0);
-
-        SmartDashboard.putNumber("LimelightX", x);
-        SmartDashboard.putNumber("LimelightY", y);
-        SmartDashboard.putNumber("LimelightArea", area);
+        //Check if limelight sees a target
+        if(tv.getDouble(0.0) == 1.0){
+            x = tx.getDouble(0.0);
+            y = ty.getDouble(0.0);
+            area = ta.getDouble(0.0);
+        }else{
+            //there is no valid target so set all values to 0.0
+            x = 0.0;
+            y = 0.0;
+            area = 0.0;
+        }
     }
-    //post to smart dashboard periodically
+
     /*
     SmartDashboard.putNumber("LimelightX", x);
     SmartDashboard.putNumber("LimelightY", y);
@@ -45,5 +56,10 @@ public class Limelight {
 
     NetworkTableInstance.getDefault().getTable("limelight").getEntry("<variablename>").setNumber(<value>);
     */
+    public static void outputToSmartDash(){
+            SmartDashboard.putNumber("LimelightX", x);
+            SmartDashboard.putNumber("LimelightY", y);
+            SmartDashboard.putNumber("LimelightArea", area);
+    }
 
 }
