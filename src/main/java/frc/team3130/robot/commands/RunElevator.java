a2c64b5edcc30fbf394aa2e3d46cfddbacd38bc7
RePiove <renatopiovesan1307@gmail.com>
2019-02-03 00:15:20 -0600

    Created RunElevator command

---
+++ src/main/java/frc/team3130/robot/commands/RunElevator.java
@@ -0,0 +1,55 @@
+package org.usfirst.frc.team3130.robot.commands;
+
+import org.usfirst.frc.team3130.robot.OI;
+import org.usfirst.frc.team3130.robot.RobotMap;
+import org.usfirst.frc.team3130.robot.subsystems.Elevator;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Preferences;
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class RunElevator extends Command {
+    private boolean changeHeight = false;
+
+    public RunElevator() {
+        requires(Elevator.GetInstance());
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+        DriverStation.reportWarning("RunElevator.java command started", false);
+        changeHeight = false;
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+        double stick = OI.gamepad.getRawAxis(RobotMap.LST_AXS_RJOYSTICKY);
+        if (Math.abs(stick) >= 0.04 ){
+            double moveSpeed = (Preferences.getInstance().getDouble("ElevatorSpeed", 0.8)) * -stick;
+            Elevator.runElevator(moveSpeed);
+            changeHeight = true;
+        } else if (changeHeight){
+            Elevator.holdHeight();
+            changeHeight = false;
+        }
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+        Elevator.resetElevator();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+        end();
+    }
+}
