068c3fdf492c68c44dd84abfde1ae188754b5c26
TomasValdes <33334918+TomasValdes@users.noreply.github.com>
2019-02-12 20:42:30 -0600

    Created commands to test intake
    Nothing in them yet

---
+++ src/main/java/frc/team3130/robot/commands/TestWrist.java
@@ -0,0 +1,34 @@
+package frc.team3130.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class TestWrist extends Command {
+    public TestWrist() {
+        //Put in the instance of whatever subsystem u need here
+        //requires();
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return true;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
