0dec8d5e11ba110a7528623eb70727d0fa6a040d
RePiove <renatopiovesan1307@gmail.com>
2019-02-17 21:02:09 -0600

    Fixed stuff in OI

--- src/main/java/frc/team3130/robot/OI.java
+++ src/main/java/frc/team3130/robot/OI.java
@@ -94,7 +94,7 @@ public class OI {
         deployClimber = new JoystickButton(weaponsGamepad, 8);
 
         testElevator = new JoystickButton(weaponsGamepad, 2);
-        testElevatorDown = new JoystickButton(weaponsGamepad, 3);
+        testElevatorDown = new JoystickButton(weaponsGamepad, RobotMap.LST_BTN_Y);
         testArm = new JoystickButton(weaponsGamepad, RobotMap.LST_BTN_A);
 
         intakeCargo = new JoystickButton(driverGamepad, 2);
@@ -113,7 +113,7 @@ public class OI {
         intakeCargo.whenPressed(new WristCargo());
 
         testElevator.whileHeld(new TestElevator());
-        testElevatorDown.whileHeld(new TestElevatorDown())
+        testElevatorDown.whileHeld(new TestElevatorDown());
         testArm.whileHeld(new TestArm());
 
     }
