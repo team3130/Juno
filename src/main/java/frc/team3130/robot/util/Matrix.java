5a5a475e71bf0139eeca46c02ea4af4c661872f0
Mikhail Kyraha <mikhail@kyraha.com>
2019-03-20 22:01:45 -0500

    Custom Matrix class for matrix multiplications

---
+++ src/main/java/frc/team3130/robot/util/Matrix.java
@@ -0,0 +1,64 @@
+package frc.team3130.robot.util;
+
+public class Matrix {
+    Double[][] body;
+    int nRows, nCols;
+
+    public Matrix(int nRows, int nCols) {
+        this.nRows = nRows;
+        this.nCols = nCols;
+        this.body = new Double[nRows][nCols];
+    }
+
+    public Matrix(Double[][] values) {
+        this.nRows = values.length;
+        this.nCols = values[0].length;
+        for (int i = 0; i < this.nRows; ++i) {
+            if (values[i].length != nCols) {
+                throw new IllegalArgumentException("Row " + i + " has " + values[i].length + ". Should be " + nCols);
+            }
+        }
+        this.body = values;
+    }
+
+    public Double get(int i, int j) {
+        return this.body[i][j];
+    }
+
+    public void put(int i, int j, Double a) {
+        this.body[i][j] = a;
+    }
+
+    public Matrix multiply(Matrix other) {
+        if (this.nCols != other.nRows) {
+            throw new IllegalArgumentException("A:nCols: " + this.nCols + " did not match B:nRows " + other.nRows + ".");
+        }
+        Matrix C = new Matrix(this.nRows, other.nCols);
+        for (int i = 0; i < this.nRows; i++) { // aRow
+            for (int j = 0; j < other.nCols; j++) { // bColumn
+                Double sum = 0.0;
+                for (int k = 0; k < this.nCols; k++) { // aColumn
+                    sum += this.body[i][k] * other.body[k][j];
+                }
+                C.body[i][j] = sum;
+            }
+        }
+        return C;
+    }
+
+    // This main() is just a unit test. Should print out an identity matrix.
+    public static void main(String[] args) {
+
+        Double[][] A = { { 4.00, 3.00 }, { 2.00, 1.00 } };
+        Double[][] B = { { -0.500, 1.500 }, { 1.000, -2.0000 } };
+        Matrix matrixA = new Matrix(A);
+        Matrix matrixB = new Matrix(B);
+        Matrix result = matrixA.multiply(matrixB);
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++)
+                System.out.print(result.get(i, j) + " ");
+            System.out.println();
+        }
+    }
+}
