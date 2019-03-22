f8e0cdad3cb236eed02768be8483fa0c57581d6d
Tantan4321 <kupa528@yahoo.com>
2019-03-21 22:05:49 -0500

    Set arm to homing angle after requested zero
    - since hardlimit is the home, do this

--- src/main/java/frc/team3130/robot/commands/Arm/ZeroArm.java
+++ src/main/java/frc/team3130/robot/commands/Arm/ZeroArm.java
@@ -39,7 +39,7 @@ public class ZeroArm extends Command {
         if(!Arm.hasBeenZeroed()){ //Limit switch was never triggered, assume the Wrist made it all the way back to 90 degrees
             Arm.zeroSensors(RobotMap.kWristBackwardMax);
         }
-        Arm.setWristRelativeAngle(90.0); //Go to stowed position
+        Arm.setWristRelativeAngle(RobotMap.kWristHomingAngle); //Go to stowed position
     }
 
     // Called when another command which requires one or more of the same
