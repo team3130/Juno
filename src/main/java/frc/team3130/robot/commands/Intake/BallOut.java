83467ce37205e68e0172515d7e9f3bd2ee0c225e
Tantan4321 <kupa528@yahoo.com>
2019-03-21 22:16:30 -0500

    Max out ball out speed

--- src/main/java/frc/team3130/robot/commands/Intake/BallOut.java
+++ src/main/java/frc/team3130/robot/commands/Intake/BallOut.java
@@ -17,7 +17,7 @@ public class BallOut extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-        Intake.runBallIntake(-0.6);
+        Intake.runBallIntake(-1.0);
     }
 
     // Make this return true when this Command no longer needs to run execute()
