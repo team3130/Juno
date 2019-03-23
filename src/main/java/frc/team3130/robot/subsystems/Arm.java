cb2c6a789e8babf491e65df77b90186c41d7a0f0
Tantan4321 <kupa528@yahoo.com>
2019-03-22 20:23:59 -0500

    Only useful smartdash outputs

--- src/main/java/frc/team3130/robot/subsystems/Arm.java
+++ src/main/java/frc/team3130/robot/subsystems/Arm.java
@@ -267,9 +267,9 @@ public class Arm extends Subsystem {
     }
 
     public void outputToSmartDashboard() {
-        SmartDashboard.putNumber("Wrist Velocity", m_wrist.getSelectedSensorVelocity(0));
+        //SmartDashboard.putNumber("Wrist Velocity", m_wrist.getSelectedSensorVelocity(0));
 
-        SmartDashboard.putNumber("Wrist current", m_wrist.getOutputCurrent() );
+        //SmartDashboard.putNumber("Wrist current", m_wrist.getOutputCurrent() );
 
         SmartDashboard.putNumber("Wrist RelAngle", getRelativeWristAngle());
 
@@ -279,11 +279,11 @@ public class Arm extends Subsystem {
 
         SmartDashboard.putNumber("Wrist Output %", m_wrist.getMotorOutputPercent());
 
-        SmartDashboard.putNumber("Wrist Current Trajectory Point", wristPeriodicIO.active_trajectory_position);
+        //SmartDashboard.putNumber("Wrist Current Trajectory Point", wristPeriodicIO.active_trajectory_position);
 
         SmartDashboard.putNumber("Wrist Feed Forward", wristPeriodicIO.feedforward);
 
-        //SmartDashboard.putNumber("Wrist Active Arbitrary FF", m_wrist.getActiveTrajectoryArbFeedFwd(0));
+        SmartDashboard.putNumber("Wrist Active Arbitrary FF", m_wrist.getActiveTrajectoryArbFeedFwd(0));
     }
 
     private enum WristControlState{
