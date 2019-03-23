feec07c66e3ef8b0f1e50ec55cba8dd892497ec5
Tantan4321 <kupa528@yahoo.com>
2019-03-23 15:46:30 -0500

    Add tuning path

--- src/main/java/frc/team3130/robot/RobotMap.java
+++ src/main/java/frc/team3130/robot/RobotMap.java
@@ -21,6 +21,7 @@ public class RobotMap {
 
 	//Chassis
 		public static double kChassisWidth = 23.5; //Checked 2/12 Distance between the left and right middle wheels
+		public static double kChassisLengthBumpers = 39.0; //Checked 3/23
 		public static double kLWheelDiameter = 6.0; //Center wheel
 		public static double kRWheelDiameter = 6.0; //Center wheel
 
@@ -35,10 +36,10 @@ public class RobotMap {
 		public static double kMPChassisP = 3.0; //TODO
 		public static double kMPChassisI = 0.0; //TODO
 		public static double kMPChassisD = 0.0; //TODO
-		public static double kMPChassisF = 1023.0 / 24980.0; //TODO
+		public static double kMPChassisF = 1.0 / 24980.0; //TODO
 
 		public static double kMPMaxVel = 115.0; //maximum achievable velocity of the drivetrain in in/s NOTE: the actual motion profile should be generated at 80% of this
-		public static double kMPMaxAcc = 30.0; ///maximum achievable acceleration of the drivetrain in in/s^2 NOTE: the actual motion profile should be generated at 80% of this
+		public static double kMPMaxAcc = 60.0; ///maximum achievable acceleration of the drivetrain in in/s^2 NOTE: the actual motion profile should be generated at 80% of this
 
 		public static double kDriveCodesPerRev = 4096;
 		public static double kDistanceToEncoder = kDriveCodesPerRev / (Math.PI * 0.5*(kLWheelDiameter+kRWheelDiameter));
