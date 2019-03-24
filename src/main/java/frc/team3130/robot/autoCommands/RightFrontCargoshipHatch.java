12ab2d96acdf4993bba35d0d80b5b01ee22197d5
Tantan4321 <kupa528@yahoo.com>
2019-03-24 17:23:05 -0500

    Set elevator to preset height at start of auton

--- src/main/java/frc/team3130/robot/autoCommands/RightFrontCargoshipHatch.java
+++ src/main/java/frc/team3130/robot/autoCommands/RightFrontCargoshipHatch.java
@@ -1,7 +1,9 @@
 package frc.team3130.robot.autoCommands;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import frc.team3130.robot.RobotMap;
 import frc.team3130.robot.commands.Chassis.OpenLoopDrive;
+import frc.team3130.robot.commands.Elevator.ElevatorToHeight;
 import frc.team3130.robot.commands.Groups.DepositHatch;
 import frc.team3130.robot.subsystems.Chassis;
 import frc.team3130.robot.subsystems.Elevator;
@@ -14,6 +16,7 @@ import frc.team3130.robot.tantanDrive.RunMotionProfile;
  */
 public class RightFrontCargoshipHatch extends CommandGroup {
 
+	private ElevatorToHeight elevatorToHeight;
 	private RunMotionProfile toCargoship;
 	private DepositHatch depositHatch;
 	private OpenLoopDrive backUp;
@@ -23,11 +26,12 @@ public class RightFrontCargoshipHatch extends CommandGroup {
 		requires(Intake.GetInstance());
 		requires(Elevator.GetInstance());
 		
-
+		elevatorToHeight = new ElevatorToHeight(RobotMap.Presets.LowestTongue.getHeight());
 		toCargoship = new RunMotionProfile(new RightCargoF());
 		depositHatch = new DepositHatch();
 		backUp = new OpenLoopDrive(-0.5,-0.5, 0.7);
-		
+
+		addParallel(elevatorToHeight, 3);
 		addSequential(toCargoship, 5);
 		addSequential(depositHatch, 2);
 		addSequential(backUp, 1);
